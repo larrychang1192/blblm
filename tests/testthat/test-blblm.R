9964702a134dfd5e9590274bc60ae0a68df4c414
Larry Chang <larrychang1118@Larrys-MacBook-Pro.local>
2020-06-10 23:49:29 -0700

    Update

--- tests/testthat/test-blblm.R
+++ tests/testthat/test-blblm.R
@@ -2,5 +2,5 @@ test_that("blblm works", {
   fit<-blblm_par(mpg ~ wt * hp, data= mtcars, m=3, B=100,nCluster = 4)
   expect_s3_class(fit, "blblm")
   cfs <- coef(fit)
-  expect_equal(length(cfs), m)
+  expect_equal(length(cfs), 3)
 })
